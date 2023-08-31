e42088ee216e2b23a0444d8b8019fdd75affe083
Henrique Naylor Lopes <h.lopes@panini.com.br>
2023-08-31 18:20:17 -0300

    fixed routes file error

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,6 @@
 import express from 'express';
 
 import {
-    getUserById,
     getUsers
 } from '../controllers/users.js';
 
@@ -9,6 +8,5 @@ const router = express.Router()
 
 router.get('/', getUsers)
 
-router.get('/:id', getUserById)
 
 export default router;
