2b7d8dd50da57b1949ac367a367f3cdaceb7bd0c
Henrique Naylor Lopes <h.lopes@panini.com.br>
2023-09-12 12:58:58 -0300

    installed EJS and created homepage route for documentation

--- index.js
+++ index.js
@@ -3,15 +3,20 @@ import bodyParser from 'body-parser'
 import dotenv from 'dotenv'
 import customersRoutes from './routes/customer.js'
 import usersRoutes from './routes/users.js'
+import homepageRoute from './routes/homepage.js'
 dotenv.config()
 
 const app = express();
-const PORT = 6000;
+const PORT = 6500;
 
 app.use(bodyParser.json());
 
 app.use('/customers', customersRoutes)
 
 app.use('/users', usersRoutes)
+
+app.use('/', homepageRoute);
+
+app.use(express.static("public"))
 
 app.listen(PORT, () => console.log(`Server running on port: http://localhost:${PORT}`));
