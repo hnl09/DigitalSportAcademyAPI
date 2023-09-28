3cbb157145cdb2e6ad08603c9229db1fbe40e5f4
Henrique Naylor Lopes <h.lopes@panini.com.br>
2023-09-28 11:27:11 -0300

    added questions controllers and routes, and questions.json file with questions data

--- controllers/questions.js
+++ controllers/questions.js
@@ -32,3 +32,35 @@ export const getQuestions = async (req, res) => {
         console.error('Error fetching questions:', error);
     }
 }
+
+export const getQuestionById = async (req, res) => {
+    const { id } = req.params
+    try {
+        const docRef = doc(db, collectionName, id)
+        const docSnap = await getDoc(docRef);
+        if (docSnap.exists()) {
+            let questionData = docSnap.data()
+            res.send(questionData)
+        } else {
+            res.send("Document does not exist")
+        }
+    } catch (error) {
+        res.status(500).send(errorMsg);
+        console.error('Error fetching question:', error);
+    }
+}
+
+export const createQuestions = async (req, res) => {
+    try {
+      const questions = req.body;
+  
+      for (const question of questions) {
+        await addDoc(collection(db, collectionName), question);
+      }
+  
+      res.status(201).json({ message: 'Questions added successfully' });
+    } catch (error) {
+      console.error('Error adding questions:', error);
+      res.status(500).json({ error: 'Failed to add questions' });
+    }
+  };
--- routes/questions.js
+++ routes/questions.js
@@ -1,10 +1,14 @@
 import express from 'express';
 
 import { authenticateToken } from '../controllers/users.js';
-import { getQuestions } from '../controllers/questions.js';
+import { getQuestions, getQuestionById, createQuestions } from '../controllers/questions.js';
 
 const router = express.Router()
 
 router.get('/', authenticateToken, getQuestions)
+
+router.get('/:id', authenticateToken, getQuestionById)
+
+router.post('/createQuestions', authenticateToken, createQuestions)
 
 export default router
